388c885285de86ed8f88525a9429fb22bc74cafb
Albert Serra <albertserra@hotmail.co.uk>
2013-07-28 13:23:58 +0100

    Terrain collison detector

--- src/js/plane3d.js
+++ src/js/plane3d.js
@@ -5,18 +5,21 @@ coroSite.Terrain = function(settings){
 	var width=settings.width;
 	var depth=settings.depth;
 	var heightData;
+	var mapData;
 	var init = function(){
-		heightData =  generateHeight( width, depth );
+		mapData =  generateHeight( width, depth );
+		heightData=[];
+		for(var i=0;mapData[i];i++){
+			heightData[i]=mapData[i]*10;
+		}
 	}
 	thiz.getHeight = function(x,z){
 		return thiz.getHeightById(x+z*depth);
 	}
 	thiz.getHeightById = function(i){
-		return heightData[i]*10;
+		return heightData[i]||0;
 	}
-	thiz.getMesh = function(){
-	
-		console.log(width+","+depth);
+	thiz.getMesh = function(){	
 		geometry = new THREE.PlaneGeometry( 7500, 7500, width - 1, depth - 1 );
 		geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
@@ -24,8 +27,7 @@ coroSite.Terrain = function(settings){
 			geometry.vertices[ i ].y = thiz.getHeightById(i);
 		}
 
-		
-		texture = new THREE.Texture( generateTexture( heightData, width, depth ), new THREE.UVMapping(), THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping );
+		texture = new THREE.Texture( generateTexture( mapData, width, depth ), new THREE.UVMapping(), THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping );
 		texture.needsUpdate = true;
 
 		mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
@@ -99,59 +101,63 @@ var plane3d = function(settings){
 	var camera, controls, scene, renderer;
 	var  texture;
 	var worldWidth = 256, worldDepth = 256, worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2;
-	var clock = new THREE.Clock();
-	container = document.getElementById( 'container' );
-
-	camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
-	
-	controls = new THREE.FirstPersonControls( camera );
-	controls.movementSpeed = 1000;
-	controls.lookSpeed = 0.1;
-	scene = new THREE.Scene();
-	
-	var terrain = new coroSite.Terrain({width:worldWidth,depth:worldDepth});
-	mesh = terrain.getMesh();
-	camera.position.y = 600;
-	scene.add( mesh );
-	
-	cube = new THREE.Mesh( new THREE.CubeGeometry( 50, 50, 50 ), new THREE.MeshNormalMaterial() );
-	scene.add( cube );
-	cube.position.x=0;
-	cube.position.y=500;
-	cube.position.z=0;
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	container.innerHTML = "";
-	container.appendChild( renderer.domElement );
-	var isAbove = true;
+	var clock, isAbove;
+	var terrain;
+	var youAreDeadToMeVaraible=false;
+	var init = function(){
+		clock = new THREE.Clock();
+		isAbove = true;
+		container = document.getElementById( 'container' );
+		camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
+		controls = new THREE.FirstPersonControls( camera,undefined, colisionDetector);
+		controls.movementSpeed = 1000;
+		controls.lookSpeed = 0.1;
+		scene = new THREE.Scene();
+		terrain = new coroSite.Terrain({width:worldWidth,depth:worldDepth});
+		mesh = terrain.getMesh();
+		camera.position.y = 600;
+		scene.add( mesh );
+		/*cube = new THREE.Mesh( new THREE.CubeGeometry( 50, 50, 50 ), new THREE.MeshNormalMaterial() );
+		scene.add( cube );
+		cube.position.x=0;
+		cube.position.y=500;
+		cube.position.z=0;*/
+		renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		container.innerHTML = "";
+		container.appendChild( renderer.domElement );
+		render();
+	}
 	var colisionDetector = function(){
 		var xMap = (camera.position.x+3750)*256/7500;
 		var zMap = (camera.position.z+3750)*256/7500;
-		var yMap = geometry.vertices[Math.floor(xMap+zMap*256)].y;
+		var yMap = terrain.getHeight(Math.floor(xMap),Math.floor(zMap));
 		var yCamera = camera.position.y;
 		if(yMap){
-			if(yCamera-yMap>0 != isAbove){
-				isAbove=!isAbove;
-				console.log("Is above: "+isAbove);
+			isAbove = yCamera-yMap>0;
+			if(!isAbove){
+				console.log("Collision! you are dead! "+yCamera+" = "+yMap);
+				youAreDeadToMeVaraible=true;
 			}
-			cube.position.x=camera.position.x;
+			/*cube.position.x=camera.position.x;
 			cube.position.y=yMap;
-			cube.position.z=camera.position.z;
+			cube.position.z=camera.position.z;*/
 			//console.log(yCamera+" = "+yMap+" " + (yCamera-yMap>0 ? "Above" : "Under") );
 		}else{
-			//console.log(xMap+" - "+zMap+" you are out of the MAP!");
+			console.log(xMap+" - "+zMap+" you are out of the MAP!");
 		}
-		
 	}
-		
 	var render = function () {
-		requestAnimationFrame(render);
-		colisionDetector();
-		controls.update( clock.getDelta() );
-		renderer.render(scene, camera);
+		if(!youAreDeadToMeVaraible){
+			requestAnimationFrame(render);
+			//colisionDetector();
+			controls.update( clock.getDelta() );
+			if(!youAreDeadToMeVaraible){
+				renderer.render(scene, camera);
+			}
+		}
 	};
-
-	render();
+	init();
 };
 
 			
@@ -159,7 +165,6 @@ var plane3d = function(settings){
 			// http://mrl.nyu.edu/~perlin/noise/
 
 var ImprovedNoise = function () {
-
 	var p = [151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,142,8,99,37,240,21,10,
 		 23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,203,117,35,11,32,57,177,33,88,237,149,56,87,
 		 174,20,125,136,171,168,68,175,74,165,71,134,139,48,27,166,77,146,158,231,83,111,229,122,60,211,
@@ -170,51 +175,30 @@ var ImprovedNoise = function () {
 		 178,185,112,104,218,246,97,228,251,34,242,193,238,210,144,12,191,179,162,241,81,51,145,235,249,
 		 14,239,107,49,192,214,31,181,199,106,157,184,84,204,176,115,121,50,45,127,4,150,254,138,236,205,
 		 93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180];
-
 	for (var i=0; i < 256 ; i++) {
-
 		p[256+i] = p[i];
-
 	}
-
 	function fade(t) {
-
 		return t * t * t * (t * (t * 6 - 15) + 10);
-
 	}
-
 	function lerp(t, a, b) {
-
 		return a + t * (b - a);
-
 	}
-
 	function grad(hash, x, y, z) {
-
 		var h = hash & 15;
 		var u = h < 8 ? x : y, v = h < 4 ? y : h == 12 || h == 14 ? x : z;
 		return ((h&1) == 0 ? u : -u) + ((h&2) == 0 ? v : -v);
-
 	}
-
 	return {
-
 		noise: function (x, y, z) {
-
 			var floorX = ~~x, floorY = ~~y, floorZ = ~~z;
-
 			var X = floorX & 255, Y = floorY & 255, Z = floorZ & 255;
-
 			x -= floorX;
 			y -= floorY;
 			z -= floorZ;
-
 			var xMinus1 = x -1, yMinus1 = y - 1, zMinus1 = z - 1;
-
 			var u = fade(x), v = fade(y), w = fade(z);
-
 			var A = p[X]+Y, AA = p[A]+Z, AB = p[A+1]+Z, B = p[X+1]+Y, BA = p[B]+Z, BB = p[B+1]+Z;
-
 			return lerp(w, lerp(v, lerp(u, grad(p[AA], x, y, z), 
 							grad(p[BA], xMinus1, y, z)),
 						lerp(u, grad(p[AB], x, yMinus1, z),
@@ -223,288 +207,171 @@ var ImprovedNoise = function () {
 							grad(p[BA+1], xMinus1, y, z-1)),
 						lerp(u, grad(p[AB+1], x, yMinus1, zMinus1),
 							grad(p[BB+1], xMinus1, yMinus1, zMinus1))));
-
 		}
 	}
 }
-			
 
-
-THREE.FirstPersonControls = function ( object, domElement ) {
-
+THREE.FirstPersonControls = function ( object, domElement , callbackMove ) {
 	this.object = object;
 	this.target = new THREE.Vector3( 0, 0, 0 );
-
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
-
 	this.movementSpeed = 1.0;
 	this.lookSpeed = 0.005;
-
 	this.lookVertical = true;
 	this.autoForward = false;
-
 	this.activeLook = true;
-
 	this.heightSpeed = false;
 	this.heightCoef = 1.0;
 	this.heightMin = 0.0;
 	this.heightMax = 1.0;
-
 	this.constrainVertical = false;
 	this.verticalMin = 0;
 	this.verticalMax = Math.PI;
-
 	this.autoSpeedFactor = 0.0;
-
 	this.mouseX = 0;
 	this.mouseY = 0;
-
 	this.lat = 0;
 	this.lon = 0;
 	this.phi = 0;
 	this.theta = 0;
-
 	this.moveForward = false;
 	this.moveBackward = false;
 	this.moveLeft = false;
 	this.moveRight = false;
 	this.freeze = false;
-
 	this.mouseDragOn = false;
-
 	this.viewHalfX = 0;
 	this.viewHalfY = 0;
-
 	if ( this.domElement !== document ) {
-
 		this.domElement.setAttribute( 'tabindex', -1 );
-
 	}
-
-	//
-
 	this.handleResize = function () {
-
 		if ( this.domElement === document ) {
-
 			this.viewHalfX = window.innerWidth / 2;
 			this.viewHalfY = window.innerHeight / 2;
-
 		} else {
-
 			this.viewHalfX = this.domElement.offsetWidth / 2;
 			this.viewHalfY = this.domElement.offsetHeight / 2;
-
 		}
-
 	};
-
 	this.onMouseDown = function ( event ) {
-
 		if ( this.domElement !== document ) {
-
 			this.domElement.focus();
-
 		}
-
 		event.preventDefault();
 		event.stopPropagation();
-
 		if ( this.activeLook ) {
-
 			switch ( event.button ) {
-
 				case 0: this.moveForward = true; break;
 				case 2: this.moveBackward = true; break;
-
 			}
-
 		}
-
 		this.mouseDragOn = true;
-
 	};
-
 	this.onMouseUp = function ( event ) {
-
 		event.preventDefault();
 		event.stopPropagation();
-
 		if ( this.activeLook ) {
-
 			switch ( event.button ) {
-
 				case 0: this.moveForward = false; break;
 				case 2: this.moveBackward = false; break;
-
 			}
-
 		}
-
 		this.mouseDragOn = false;
-
 	};
-
 	this.onMouseMove = function ( event ) {
-
 		if ( this.domElement === document ) {
-
 			this.mouseX = event.pageX - this.viewHalfX;
 			this.mouseY = event.pageY - this.viewHalfY;
-
 		} else {
-
 			this.mouseX = event.pageX - this.domElement.offsetLeft - this.viewHalfX;
 			this.mouseY = event.pageY - this.domElement.offsetTop - this.viewHalfY;
-
 		}
-
 	};
-
 	this.onKeyDown = function ( event ) {
-
 		//event.preventDefault();
-
 		switch ( event.keyCode ) {
-
 			case 38: /*up*/
 			case 87: /*W*/ this.moveForward = true; break;
-
 			case 37: /*left*/
 			case 65: /*A*/ this.moveLeft = true; break;
-
 			case 40: /*down*/
 			case 83: /*S*/ this.moveBackward = true; break;
-
 			case 39: /*right*/
 			case 68: /*D*/ this.moveRight = true; break;
-
 			case 82: /*R*/ this.moveUp = true; break;
 			case 70: /*F*/ this.moveDown = true; break;
-
 			case 81: /*Q*/ this.freeze = !this.freeze; break;
-
 		}
-
 	};
-
 	this.onKeyUp = function ( event ) {
-
 		switch( event.keyCode ) {
-
 			case 38: /*up*/
 			case 87: /*W*/ this.moveForward = false; break;
-
 			case 37: /*left*/
 			case 65: /*A*/ this.moveLeft = false; break;
-
 			case 40: /*down*/
 			case 83: /*S*/ this.moveBackward = false; break;
-
 			case 39: /*right*/
 			case 68: /*D*/ this.moveRight = false; break;
-
 			case 82: /*R*/ this.moveUp = false; break;
 			case 70: /*F*/ this.moveDown = false; break;
-
 		}
-
 	};
-
 	this.update = function( delta ) {
-
 		if ( this.freeze ) {
-
 			return;
-
 		}
-
 		if ( this.heightSpeed ) {
-
 			var y = THREE.Math.clamp( this.object.position.y, this.heightMin, this.heightMax );
 			var heightDelta = y - this.heightMin;
-
 			this.autoSpeedFactor = delta * ( heightDelta * this.heightCoef );
-
 		} else {
-
 			this.autoSpeedFactor = 0.0;
-
 		}
-
 		var actualMoveSpeed = delta * this.movementSpeed;
-
 		if ( this.moveForward || ( this.autoForward && !this.moveBackward ) ) this.object.translateZ( - ( actualMoveSpeed + this.autoSpeedFactor ) );
 		if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
-
 		if ( this.moveLeft ) this.object.translateX( - actualMoveSpeed );
 		if ( this.moveRight ) this.object.translateX( actualMoveSpeed );
-
 		if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
 		if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
-
 		var actualLookSpeed = delta * this.lookSpeed;
-
 		if ( !this.activeLook ) {
-
 			actualLookSpeed = 0;
-
 		}
-
 		var verticalLookRatio = 1;
-
 		if ( this.constrainVertical ) {
-
 			verticalLookRatio = Math.PI / ( this.verticalMax - this.verticalMin );
-
 		}
-
 		this.lon += this.mouseX * actualLookSpeed;
 		if( this.lookVertical ) this.lat -= this.mouseY * actualLookSpeed * verticalLookRatio;
-
 		this.lat = Math.max( - 85, Math.min( 85, this.lat ) );
 		this.phi = THREE.Math.degToRad( 90 - this.lat );
-
 		this.theta = THREE.Math.degToRad( this.lon );
-
 		if ( this.constrainVertical ) {
-
 			this.phi = THREE.Math.mapLinear( this.phi, 0, Math.PI, this.verticalMin, this.verticalMax );
-
 		}
-
 		var targetPosition = this.target,
 			position = this.object.position;
-
 		targetPosition.x = position.x + 100 * Math.sin( this.phi ) * Math.cos( this.theta );
 		targetPosition.y = position.y + 100 * Math.cos( this.phi );
 		targetPosition.z = position.z + 100 * Math.sin( this.phi ) * Math.sin( this.theta );
-
 		this.object.lookAt( targetPosition );
-
+		callbackMove();
 	};
-
-
 	this.domElement.addEventListener( 'contextmenu', function ( event ) { event.preventDefault(); }, false );
-
 	this.domElement.addEventListener( 'mousemove', bind( this, this.onMouseMove ), false );
 	this.domElement.addEventListener( 'mousedown', bind( this, this.onMouseDown ), false );
 	this.domElement.addEventListener( 'mouseup', bind( this, this.onMouseUp ), false );
 	this.domElement.addEventListener( 'keydown', bind( this, this.onKeyDown ), false );
 	this.domElement.addEventListener( 'keyup', bind( this, this.onKeyUp ), false );
-
 	function bind( scope, fn ) {
-
 		return function () {
-
 			fn.apply( scope, arguments );
-
 		};
-
 	};
-
 	this.handleResize();
-
 };
-
-$(document).on('click',function(){new plane3d();});
+var planet;
+$(document).on('click',function(){if(!planet) planet=new plane3d();});
